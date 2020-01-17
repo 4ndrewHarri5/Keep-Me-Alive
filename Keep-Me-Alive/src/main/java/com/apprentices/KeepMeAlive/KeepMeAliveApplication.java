71abff58088deab5e82bc2d12ad5371e5c459d01
Andrew Harris <4ndrew.harri5@gmail.com>
2020-01-17 14:35:47 +0000

    First commit

---
+++ Keep-Me-Alive/src/main/java/com/apprentices/KeepMeAlive/KeepMeAliveApplication.java
@@ -0,0 +1,13 @@
+package com.apprentices.KeepMeAlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepMeAliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepMeAliveApplication.class, args);
+	}
+
+}
